c160434ba6a5bb232c2a4113f768a61d7694cd6a
João Marques <jotaveultra@gmail.com>
2019-06-17 17:20:39 -0300

    Test IPI properties

--- tests/IPITest.php
+++ tests/IPITest.php
@@ -26,6 +26,35 @@ class IPITest extends TestCase
     }
 
     /**
+     * Test properties types
+     */
+    public function testPropertiesTypes()
+    {
+        $ipi = $this->instantiateIPI();
+        $ipi->setCNPJProd('');
+        $ipi->setCSelo('');
+        $ipi->setQSelo(1.0);
+        $ipi->setCEnq('');
+        $ipi->setCST('');
+        $ipi->setVBC(1.0);
+        $ipi->setPIPI(1.0);
+        $ipi->setVIPI(1.0);
+        $ipi->setQUnid(1.0);
+        $ipi->setVUnid(1.0);
+
+        $this->assertIsString($ipi->getCNPJProd());
+        $this->assertIsString($ipi->getCSelo());
+        $this->assertIsFloat($ipi->getQSelo());
+        $this->assertIsString($ipi->getCEnq());
+        $this->assertIsString($ipi->getCST());
+        $this->assertIsFloat($ipi->getVBC());
+        $this->assertIsFloat($ipi->getPIPI());
+        $this->assertIsFloat($ipi->getVIPI());
+        $this->assertIsFloat($ipi->getQUnid());
+        $this->assertIsFloat($ipi->getVUnid());
+    }
+
+    /**
      * Instantiate and return an IPI object
      */
     private function instantiateIPI()
